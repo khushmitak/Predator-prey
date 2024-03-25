d21ee73f95b4293550cbcbfb1c4ccae7718275e4
Khushmita Kaur <khushp9894@gmail.com>
2024-03-24 22:30:58 -0700

    add field class

--- antbug.cpp
+++ antbug.cpp
@@ -10,3 +10,21 @@ const int ANT = 1;
 const int DOODLEBUG = 2;
 const int ANTS_NUM = 100; // the field is initialized with 100 ants
 const int DOODLEBUGS_NUM = 5; // the field is initialized with 5 doodlebugs
+
+class Field { 
+public:
+    void setOrganisms(); 
+    void startSimulation(); 
+    void printOutput(); 
+    int getTimeSteps() const {return timeSteps;}
+    int getRandomNumber(int highestValue, int supplementary = 0) {return ((rand() % highestValue) + supplementary);}
+    Field(int doodleBugsCount, int antsCount) : doodlebugsNum(doodleBugsCount), timeSteps(0),
+    antsNum(antsCount) {this->setOrganisms();}
+    Field() : doodlebugsNum(DOODLEBUGS_NUM), antsNum(ANTS_NUM), timeSteps(0) {this->setOrganisms();}
+    ~Field();
+    Organism* grid[SIZE][SIZE];
+private:
+    int antsNum;
+    int doodlebugsNum;
+    int timeSteps;
+};
