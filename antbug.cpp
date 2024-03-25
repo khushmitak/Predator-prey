3ea88b93f96aa708e817e9b2afef832fc8bc6318
Khushmita Kaur <khushp9894@gmail.com>
2024-03-24 22:30:24 -0700

    add variables

---
+++ antbug.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <vector>
+using namespace std;
+
+const int SIZE = 20; // size * size grid of cells in the world
+const int ANT = 1;
+const int DOODLEBUG = 2;
+const int ANTS_NUM = 100; // the field is initialized with 100 ants
+const int DOODLEBUGS_NUM = 5; // the field is initialized with 5 doodlebugs
